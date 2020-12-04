8b6c142a115f37843862670dc72f623a48ce9bf9
priteech <53416442+priteech@users.noreply.github.com>
2020-12-03 18:04:09 -0600

    State,Props, data transfer Child-P

--- test-project/src/App.js
+++ test-project/src/App.js
@@ -1,25 +1,196 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
+
+function  Header(props){
+  return(
+    <div>
+      <h1>{props.name}  Pritee Chuaurasiya</h1>
+    </div>
+  )
+}
+
+function  Main(props){
+  return(
+    <div>
+      <h1 style={{color:"blue"}}>this is Head</h1>
+  <ul>
+    {props.courses.map((course,i)=> <li key={i}>{course}</li>)}
+    </ul>
+    </div>
+  )
+}
+function  Footer(){
+  return(
+    <div>
+      <h1>this is footer</h1>
+    </div>
+  )
+}
+
+const courses = [
+  "Angular",
+  "React",
+  "Java",
+  "Dotnet",
+"RDBMS"
+]
+const courseObjects = courses.map((course,i) => ({id:i,title:course}))
+console.log(courseObjects);
+// courses.map((course) =>console.log(course))
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="My Name is"/>
+      <Main courses ={courses}/>
+      <Footer/>
+      <Employee
+      Id="101"
+      Name="Ram"
+      Location="channai"
+      Salary = "70000"
+      DeptName="Training"
+      HeadName="Prasad"
+      BasicSalary="10000"
+      HRA = "1500"
+      SpecialAllowence="25000"
+
+
+      />
+      <Student/>
+      <CountCharectre/>
+     
     </div>
   );
 }
+
+class Employee extends React.Component
+{
+  constructor(props){
+    super(props)
+    console.log(props)
+  }
+
+  render(){
+    return<div>
+      <h2 align="center">Employee Details</h2>
+      <table border="1" width="70%"  align="center">
+        <tbody>
+<tr>
+  <th>ID</th>
+  <th>{this.props.Id}</th>
+</tr>
+<tr>
+<th>Name</th>
+  <th>{this.props.Name}</th>
+</tr>
+<tr>
+  <th>Salary</th>
+  <th>{this.props.Salary}</th>
+</tr>
+        </tbody>
+        </table> 
+        <Department
+        DeptName={this.props.DeptName}
+        HeadName={this.props.HeadName}
+        />
+        <Salary 
+        BasicSalary={this.props.BasicSalary}
+        HRA={this.props.HRA}
+        SpecialAllowence={this.props.SpecialAllowence}
+        />
+         </div>
+
+  }
+}
+
+class Department extends React.Component{
+  render(){
+    return <div>
+
+      <h2>Department Details</h2>
+      <table border="1" width="70%" align="center">
+<tr>
+  <th>Name</th>
+  <th>{this.props.DeptName}</th>
+</tr>
+<tr>
+  <th>Head</th>
+  <th>{this.props.HeadName}</th>
+</tr>
+      </table>
+      
+    </div>
+  }
+}
+
+class Salary extends React.Component{
+  constructor(props){
+    super(props)
+    console.log(props)
+  }
+  render(){
+    return(
+<div>
+    <p>basic Salary: <b>{this.props.BasicSalary}</b></p>
+    <p>basic HRA: <b>{this.props.HRA}</b></p>
+    <p>Special Allowance: <b>{this.props.SpecialAllowence}</b></p>
+</div>
+    )
+  }
+}
+
+class Student extends React.Component{
+  counter= 0;
+addStudent = () => 
+{this.counter = this.counter+1;
+    alert("Add Student Clicked Count:"+ this.counter);
+  }
+  render(){
+    return(
+      <div>
+        <h2>Student Component</h2>
+        <button onClick={this.addStudent}>Add Student</button>
+      </div>
+    )
+  }
+}
+
+class CountCharectre extends React.Component{
+  constructor(props){
+    super(props)
+      this.state={
+        messange:"",
+        counter:10
+
+      }
+  }
+
+
+  onmassegeChange (text) {
+    this.setState({
+      messange:"Message has  "+text.length +"  Number of Characters" 
+    }
+    )
+
+  }
+  render(){
+    return(
+      <div>
+        <h2>welcome to count Characters component</h2>
+        <p><lable>Enter Messege<input type="text" 
+        onChange={e=>this.onmassegeChange(e.target.value)}
+        /></lable></p>
+        <p><label>{this.state.messange}</label></p>
+        <p><label>{this.state.counter}</label></p>
+      </div>
+    )
+  }
+}
+
 
 export default App;
--- test-project/src/index.js
+++ test-project/src/index.js
@@ -2,28 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+ import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-/* ReactDOM.render(
-<h1>Hi There, I am pritee</h1>,
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ reportWebVitals();
+ 
+/*  ReactDOM.render(
+
 document.getElementById("root")
 
-); */
+); */ 
+
+/* const element = <h1>React programing</h1>
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+) */
 
 // function component- consider as element-yeeInfo
 
-function DisplayEmployeeInfo(employee){
+/* function DisplayEmployeeInfo(employee){
 return <div>
 <p>
   <lable>ID</lable>
@@ -40,3 +48,4 @@ return <div>
 
 const element = <DisplayEmployeeInfo  Id="101" Name="Ram"></DisplayEmployeeInfo>
 ReactDOM.render(element,document.getElementById("root"));
+ */
